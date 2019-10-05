d4e7626c44e797b603ac295fb72f74ae3bdd351c
Xuejun Li <lixuejun1974@icloud.com>
2019-10-04 21:46:52 -0400

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Xuejun Li on 10/4/19.
+//  Copyright © 2019 Xuejun Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
